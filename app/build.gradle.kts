51d869494b72e41dd50ee0d69eba67cc2c42ee0e
pedrofarnetani <pedrofalterosa@gmail.com>
2025-06-03 00:04:09 -0300

    Add Hilt as dependency injection

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.dagger.hilt.android)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.kapt)
 }
 
 android {
@@ -55,6 +57,10 @@ dependencies {
     // Compose dependencies
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
+
+    // Hilt dependencies
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
 
     // Debugging and testing dependencies
     debugImplementation(libs.androidx.ui.tooling)
--- app/src/main/kotlin/com/tmdb/themoviedb/TheMovieDBApplication.kt
+++ app/src/main/kotlin/com/tmdb/themoviedb/TheMovieDBApplication.kt
@@ -1,5 +1,7 @@
 package com.tmdb.themoviedb
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class TheMovieDBApplication : Application()
